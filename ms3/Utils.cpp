ccd2c61c164e00ad8b24c2bbd0fec1e7284ca732
chienaeae <chienaeae@gmail.com>
2024-07-17 22:53:43 -0400

    feat: complete ms3

---
+++ ms3/Utils.cpp
@@ -0,0 +1,12 @@
+// Final Project Milestone 3
+// Utils Module
+// File	Utils.cpp
+// Version 1.0
+// Author	Chia-Yu Chien
+// Revision History
+// -----------------------------------------------------------
+// Name               Date                 Reason
+//
+/////////////////////////////////////////////////////////////////
+
+namespace seneca {} // namespace seneca
